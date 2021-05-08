ef5ba4354f1f54dea9ed55e5db016d4b699f3cc0
thilakshids <thilakshids@gmail.com>
2021-05-08 12:24:25 +0530

    Add Items

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        ke:''
+      }
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
   render(){
     return(
       <div className="App">
         <header>
-        <form id="to-do-form">
-          <input type="text" placeholder="Enter tex"/>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter tex"
+          value={this.state.currentItem}/>
+          onChnage={this.handleInput}
           <button type="submit">Add</button>
         </form>
       </header>
